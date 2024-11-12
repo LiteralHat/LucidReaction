5649d0100d7d265530e8c043fa6a3245350755b4
LiteralHat <90408956+LiteralHat@users.noreply.github.com>
2024-11-12 20:34:05 +1100

    tutorial stuff

---
+++ LucidReaction/src/App.tsx
@@ -0,0 +1,13 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["Potatoes", "San Francisco", "Weezer", "Hah"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
+  return <div><ListGroup items={items} heading='Words' onSelectItem={handleSelectItem} /></div>
+};
+
+export default App
---
+++ LucidReaction/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing for react lol
+function Message() {
+    //JSX: Javascript XML
+    const name = 'Charly';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
